[fix] calculation of deltas now considers hexagonality of the grid

src/engine/board.py
@@ -6,11 +6,20 @@ from typing import Iterator, Tuple
 import numpy as np
 
 Coordinate = Tuple[int, int]
+Action = Tuple[Coordinate, str]
+
 DIRECTIONS = {
-    'UR': ( 1,  1), 'UL': ( 1, -1),
+    'UR': ( 0,  1), 'UL': (-1, -1),
     'R':  ( 0,  1),  'L': ( 0, -1),
-    'DR': (-1, -1), 'DL': (-1, -1)
+    'DR': ( 0, -1), 'DL': (-1, -1)
 }
+
+def _calculate_delta(x: int, y: int, direction: str) -> Coordinate:
+    """Returns the delta of the given direction."""
+    dx, dy = DIRECTIONS[direction]
+    if y % 2 == 1:
+        dx = dx + 1
+    return dx, dy
 
 
 class Board:
@@ -61,8 +70,8 @@ class Board:
         """Returns the state of the board."""
         return self._grid
 
-    def get_score(self, player) -> int:
-        return len(self._grid[..., 0] == player)
+    def get_score(self, player_id) -> int:
+        return len(self._grid[..., 0] == player_id)
 
     def is_empty(self, x: int, y: int) -> bool:
         """Returns True if the cell is empty."""
@@ -88,15 +97,15 @@ class Board:
         assert self.is_occupied(x, y)
         return self._grid[x, y, 1] 
 
-    def get_player_positions(self, player: int) -> Iterator[Coordinate]:
+    def get_player_positions(self, player_id: int) -> Iterator[Coordinate]:
         """Returns an iterator of the positions of the
         given player."""
-        return np.where(self._grid[..., 0] == player)
+        return np.where(self._grid[..., 0] == player_id)
 
-    def initialize_player(self, player: int, x: int, y: int, n_units: int) -> None:
+    def initialize_player(self, player_id: int, x: int, y: int, n_units: int) -> None:
         """Initializes the player at the given coordinates."""
         assert self.is_empty(x, y)
-        self._grid[x, y, 0] = player  
+        self._grid[x, y, 0] = player_id 
         self._grid[x, y, 1] = n_units 
 
     def next_empty_cell(self, x: int, y: int, direction: str) -> Coordinate:
@@ -105,36 +114,51 @@ class Board:
         if direction not in DIRECTIONS.keys():
             return ValueError('Invalid direction.')
 
-        dx, dy = DIRECTIONS[direction]
+        dx, dy = _calculate_delta(x, y, direction)
         while self._grid[x+dx, y+dy, 0] == 0: 
             x += dx; y += dy
             if self._out_of_bounds(x+dx) or self._out_of_bounds(y+dy):
                 break
         return x, y
 
-    def move_player(self, player: int, x: int, y: int, n_units: int, direction: str) -> None:
+    def move_player(self, player_id: int, x: int, y: int, n_units: int, direction: str) -> None:
         """Moves the player at the given coordinates."""
-        assert self.player_at(x, y) == player
+        assert self.player_at(x, y) == player_id
         assert n_units > 1 and n_units < self.units_at(x, y) - 1
         nx, ny = self.next_empty_cell(x, y, direction)
         assert self.is_empty(nx, ny) and (nx != x or ny != ny)
 
         self._grid[x, y, 1] -= n_units      
-        self._grid[nx, ny, 0] = player   
+        self._grid[nx, ny, 0] = player_id
         self._grid[nx, ny, 1] = n_units  
 
-    def get_player_moveable_positions(self, player: int) -> Iterator[Coordinate]:
+    def get_player_moveable_positions(self, player_id: int) -> Iterator[Coordinate]:
         """Returns an iterator of the positions of the
         given player that can be moved."""
-        player_options = np.where(self._grid[..., 0] == player and self._grid[..., 1] > 1)
+        player_options = np.where(self._grid[..., 0] == player_id and self._grid[..., 1] > 1)
         return [option for option in player_options
             if self._available_moves(option)]
+
+    def _get_actions(self, player_id: int) -> Iterator[Action]:
+        """Returns an iterator of the actions that the
+        given player can perform."""
+        for x, y in self.get_player_moveable_positions(player_id):
+            for direction in DIRECTIONS.keys():
+                nx, ny = self.next_empty_cell(x, y, direction)
+                if x != nx or y != ny:
+                    yield (x, y), direction
+
+    def get_actions(self, player_id: int) -> Iterator[Action]:
+        """Returns an iterator of the actions that the
+        given player can perform."""
+        return [action for action in self._get_actions(player_id)]
 
     def _available_moves(self, x: int, y: int) -> bool:
         """Returns True if the cell can be moved."""
         # For efficiency, check neighbours first
-        for _, (nx, ny) in DIRECTIONS.items():
-            if self._grid[x+nx, y+ny] == 0: 
+        for direction in DIRECTIONS.keys():
+            dx, dy = _calculate_delta(x, y, direction)
+            if self._grid[x+dx, y+dy] == 0: 
                 return True
 
         # Then check the rest