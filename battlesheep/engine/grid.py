253f3ed19ab8199c5fa73e3cc9d4268bda0b7d3c
Carlos Outeiral <carlos@outeiral.net>
2022-12-23 18:42:02 +0000

    [dev] updated and written tests to HexagonalGrid

--- battlesheep/engine/grid.py
+++ battlesheep/engine/grid.py
@@ -60,6 +60,13 @@ class HexagonalGrid:
         s = -q - r
         return q, r, s
 
+    def to_cube(self, x: int, y: int) -> Tuple[int, int, int]:
+        """Converts offset coordinates to cube coordinates."""
+        x -= self._size // 2
+        y -= self._size // 2
+        q, r, s = HexagonalGrid.offset_to_cube(x, y)
+        return q, r, s
+
     def to_offset(self, q: int, r: int, s: int) -> Tuple[int, int]:
         """Converts cube coordinates to offset coordinates."""
         x, y = HexagonalGrid.cube_to_offset(q, r, s)
@@ -78,27 +85,31 @@ class HexagonalGrid:
 
     def is_empty(self, q: int, r: int, s:int) -> bool:
         """Returns True if the cell is empty."""
-        return self[q, r, s][0] == 0 
+        return self.__getitem__(q, r, s)[0] == 0 
 
     def is_hole(self, q: int, r: int, s: int) -> bool:
         """Returns True if the cell is a hole."""
-        return self[q, r, s][0] == -1 
+        return self.__getitem__(q, r, s)[0] == -1 
 
     def is_occupied(self, q: int, r: int, s: int) -> bool:
         """Returns True if the cell is occupied."""
-        return self[q, r, s][0] > 0 
+        return self.__getitem__(q, r, s)[0] > 0 
+
+    def is_occupied_by_player(self, q: int, r: int, s: int, player_id: int) -> bool:
+        """Returns True if the cell is occupied."""
+        return self.__getitem__(q, r, s)[0] == player_id
 
     def player_at(self, q: int, r: int, s: int) -> int:
         """Returns the player occupying the cell. Will
         raise an AssertionError if the cell is not occupied."""
         assert self.is_occupied(q, r, s)
-        return self[q, r, s][0]
+        return self.__getitem__[0]
 
     def units_at(self, q: int, r: int, s: int) -> int:
         """Returns the number of units in the cell. Will
         raise an AssertionError if the cell is not occupied."""
         assert self.is_occupied(q, r, s)
-        return self[q, r, s][1] 
+        return self.__getitem__(q, r, s)[1] 
 
     def get_player_positions(self, player_id: int) -> Iterator[Coordinate]:
         """Returns an iterator of the positions of the
@@ -110,9 +121,8 @@ class HexagonalGrid:
         """Returns the next cell in the grid in the given
         direction that a player can move to."""
         if direction not in DIRECTIONS.keys():
-            return ValueError('Invalid direction.')
+            raise ValueError('Invalid direction.')
 
-        q1, r1, s1 = q, r, s
         dq, dr, ds = DIRECTIONS[direction]
         while not self._out_of_bounds(q+dq, r+dr, s+ds):
             if not self.is_empty(q+dq, r+dr, s+ds):
@@ -120,7 +130,7 @@ class HexagonalGrid:
             q += dq
             r += dr
             s += ds
-        return q1, r1, s1
+        return q, r, s
 
     def get_moveable_positions(self) -> Iterator[Coordinate]:
         """Returns an iterator of the positions where it
@@ -160,6 +170,6 @@ class HexagonalGrid:
         """Initializes the player at the given coordinates."""
         assert self.is_empty(q, r, s)
         assert player_id >= 1
-        x, y = self.to_offset(x, y)
+        x, y = self.to_offset(q, r, s)
         self._grid[x, y, 0] = player_id 
         self._grid[x, y, 1] = n_units 
---
+++ battlesheep/engine/test_grid.py
@@ -0,0 +1,79 @@
+"""Tests for HexagonalGrid class."""
+
+import numpy as np
+
+from grid import HexagonalGrid
+
+
+def test_init():
+    square_grid = HexagonalGrid(8)
+    assert square_grid.get_size() == 8
+    assert square_grid.get_state().shape == (8, 8, 2)
+    assert square_grid.get_state().dtype == np.int8
+    assert square_grid.get_state().sum() == 0
+
+    holes = [(0, 0), (1, 1), (2, 2)]
+    square_grid = HexagonalGrid(8, holes=holes)
+    assert square_grid.get_size() == 8
+    assert square_grid.get_state().shape == (8, 8, 2)
+    assert square_grid.get_state().dtype == np.int8
+    assert square_grid.get_state().sum() == -3
+
+def test_coordinate_conversion():
+    square_grid = HexagonalGrid(8)
+    for q in range(-4, 4):
+        for r in range(-4, 4):
+            for s in range(-4, 4):
+                if q + r + s == 0:
+                    x, y = square_grid.to_offset(q, r, s)
+                    assert x >= 0 and x < 8
+                    assert y >= 0 and y < 8
+                    q_, r_, s_ = square_grid.to_cube(x, y)
+                    assert q == q_
+                    assert r == r_
+                    assert s == s_
+
+
+def test_grid_values():
+    holes = [(0, 0), (1, 1), (2, 2)]
+    square_grid = HexagonalGrid(8, holes=holes)
+    assert square_grid.is_hole(*square_grid.to_cube(0, 0))
+    assert square_grid.is_hole(*square_grid.to_cube(1, 1))
+    assert square_grid.is_hole(*square_grid.to_cube(2, 2))
+
+    q1, r1, s1 = square_grid.to_cube(1, 0)
+    q2, r2, s2 = square_grid.to_cube(5, 7)
+    square_grid.initialize_player(1, q1, r1, s1, 16)
+    square_grid.initialize_player(2, q2, r2, s2, 16)
+    assert not square_grid.is_empty(q1, r1, s1)
+    assert not square_grid.is_empty(q2, r2, s2)
+    assert not square_grid.is_hole(q1, r1, s1)
+    assert not square_grid.is_hole(q2, r2, s2)
+    assert square_grid.is_occupied(q1, r1, s1) 
+    assert square_grid.is_occupied(q2, r2, s2)
+    assert square_grid.is_occupied_by_player(q1, r1, s1, 1)
+    assert square_grid.is_occupied_by_player(q2, r2, s2, 2)
+    assert not square_grid.is_occupied_by_player(q1, r1, s1, 2)
+    assert not square_grid.is_occupied_by_player(q2, r2, s2, 1)
+    assert square_grid.units_at(q1, r1, s1) == 16
+    assert square_grid.units_at(q2, r2, s2) == 16
+
+def test_movements1():
+    square_grid = HexagonalGrid(8)
+    x1, y1 = 0, 0
+    q1, r1, s1 = square_grid.to_cube(x1, y1)
+    q2, r2, s2 = square_grid.get_next_moveable_cell(q1, r1, s1, 'R')
+    x2, y2 = square_grid.to_offset(q2, r2, s2)
+    assert x2 == 0 and y2 == 7
+
+    q2, r2, s2 = square_grid.get_next_moveable_cell(q1, r1, s1, 'L')
+    x2, y2 = square_grid.to_offset(q2, r2, s2)
+    assert x2 == x1 and y2 == y1
+
+def test_movements2():
+    square_grid = HexagonalGrid(8, holes=[(0, 4)])
+    x1, y1 = 0, 0
+    q1, r1, s1 = square_grid.to_cube(x1, y1)
+    q2, r2, s2 = square_grid.get_next_moveable_cell(q1, r1, s1, 'R')
+    x2, y2 = square_grid.to_offset(q2, r2, s2)
+    assert x2 == 0 and y2 == 3
